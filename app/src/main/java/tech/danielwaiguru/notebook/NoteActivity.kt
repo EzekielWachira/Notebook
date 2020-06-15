e373a6181ddccaa1944ace0ffc447ccd0a79e9a3
Daniel Waiguru <danielirungu91@gmail.com>
2020-06-15 15:04:19 +0300

    Added CreateNoteActivity

--- app/src/main/java/tech/danielwaiguru/notebook/NoteActivity.kt
+++ app/src/main/java/tech/danielwaiguru/notebook/NoteActivity.kt
@@ -1,11 +1,16 @@
 package tech.danielwaiguru.notebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_note.*
 
 class NoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
+        fabAddNote.setOnClickListener {
+            startActivity(Intent(this, CreateNoteActivity::class.java))
+        }
     }
 }
