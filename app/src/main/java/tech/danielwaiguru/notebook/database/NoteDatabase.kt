911472217ef71425a8847c376b351ab3a9183ac9
Daniel Waiguru <danielirungu91@gmail.com>
2020-06-16 07:37:23 +0300

    Added NoteDatabase abstract class

---
+++ app/src/main/java/tech/danielwaiguru/notebook/database/NoteDatabase.kt
@@ -0,0 +1,30 @@
+package tech.danielwaiguru.notebook.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], version = 1, exportSchema = false)
+abstract class NoteDatabase : RoomDatabase(){
+    abstract fun noteDao(): NoteDao
+    companion object{
+        @Volatile
+        private var INSTANCE: NoteDatabase? = null
+        fun getInstance(context: Context): NoteDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        NoteDatabase::class.java,
+                        "notes.db"
+                    ).build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+
+        }
+    }
+}
