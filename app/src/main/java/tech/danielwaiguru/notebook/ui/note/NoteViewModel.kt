235fde04e5be543050b5f02ca787f13fef3b14a4
Daniel Waiguru <danielirungu91@gmail.com>
2020-12-14 16:10:13 +0300

    init testing mock objects

--- app/src/main/java/tech/danielwaiguru/notebook/ui/note/NoteViewModel.kt
+++ app/src/main/java/tech/danielwaiguru/notebook/ui/note/NoteViewModel.kt
@@ -2,13 +2,11 @@ package tech.danielwaiguru.notebook.ui.note
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import org.koin.core.KoinComponent
-import org.koin.core.inject
 import tech.danielwaiguru.notebook.database.Note
 import tech.danielwaiguru.notebook.database.NoteRepository
 
-class NoteViewModel: ViewModel(), KoinComponent {
-    private val noteRepository: NoteRepository by inject()
+class NoteViewModel(noteRepository: NoteRepository): ViewModel(){
+    //private val noteRepository: NoteRepository by inject()
     val allNotes: LiveData<List<Note>> = noteRepository.allNotes
     /*init {
         val noteDao = NoteDatabase.getInstance(application).noteDao()
