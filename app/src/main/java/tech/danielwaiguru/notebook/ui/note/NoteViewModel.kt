332673ac5bd664980c8274fa0a65ed2905cf8c22
Daniel Waiguru <danielirungu91@gmail.com>
2020-12-16 07:51:34 +0300

    fix: LiveData transformation

--- app/src/main/java/tech/danielwaiguru/notebook/ui/note/NoteViewModel.kt
+++ app/src/main/java/tech/danielwaiguru/notebook/ui/note/NoteViewModel.kt
@@ -2,12 +2,13 @@ package tech.danielwaiguru.notebook.ui.note
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import tech.danielwaiguru.notebook.database.Note
 import tech.danielwaiguru.notebook.database.NoteRepository
 
 class NoteViewModel(noteRepository: NoteRepository): ViewModel(){
     //private val noteRepository: NoteRepository by inject()
-    val allNotes: LiveData<List<Note>> = noteRepository.allNotes
+    val allNotes: LiveData<List<Note>> = noteRepository.getAllNotes().asLiveData()
 
     /*init {
         val noteDao = NoteDatabase.getInstance(application).noteDao()
