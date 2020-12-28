fd01bf9bf358d0547a12825700c49b52749ce71f
Daniel Waiguru <danielirungu91@gmail.com>
2020-12-28 14:40:43 +0300

    release: generate v1.0.1 signed apk

--- app/src/test/java/tech/danielwaiguru/notebook/ui/note/NoteViewModelTest.kt
+++ app/src/test/java/tech/danielwaiguru/notebook/ui/note/NoteViewModelTest.kt
@@ -1,14 +1,15 @@
 package tech.danielwaiguru.notebook.ui.note
 
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import org.koin.test.AutoCloseKoinTest
 import org.koin.test.inject
-import tech.danielwaiguru.notebook.model.Note
 import tech.danielwaiguru.notebook.database.NoteRepository
 import tech.danielwaiguru.notebook.getOrAwaitValue
+import tech.danielwaiguru.notebook.model.Note
 import tech.danielwaiguru.notebook.ui.add.AddNoteViewModel
 import tech.danielwaiguru.notebook.ui.edit.ReadNoteViewModel
 
@@ -38,5 +39,6 @@ class NoteViewModelTest: AutoCloseKoinTest() {
                 1, "Test Note", "Test note text","16 December, 2020")
         )
         val notes = viewModel.allNotes.getOrAwaitValue()
+        Assert.assertEquals(dummyData, notes)
     }
 }
